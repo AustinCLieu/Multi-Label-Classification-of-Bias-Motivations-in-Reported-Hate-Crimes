1419d8c4da2e409cc05f0ec20ff1efc492d28cfe
AustinCLieu <asianonrice@gmail.com>
2026-03-05 14:10:27 -0800

    Started multihotencoding of SF dataset and listed columns that need to be multi encoded

---
+++ SFPreProcessing/Multihotencoding.py
@@ -0,0 +1,5 @@
+# SuspectsRaceAsAGroup
+# MostSeriousLocation separated by delimiter "/""
+# MostSeriousBias separated by delimiter "or"
+
+multi_class_cols = ["SuspectsRaceAsAGroup", "MostSeriousLocation", "MostSeriousBias"]
